278488f9193327640f1c6fde9bcf42c7b01b7452
JustNevi <slavkad25@gmail.com>
2025-04-04 15:19:29 +0300

    fix: install visit app; feat: add home page

--- qr_page/visit/urls.py
+++ qr_page/visit/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:qr_id>", views.home, name="home")
+    path("", views.home, name="home"),
+    path("<int:qr_id>", views.visit, name="visit")
 ]
--- qr_page/visit/views.py
+++ qr_page/visit/views.py
@@ -4,11 +4,13 @@ from .models import VisitPage
 
 from .componets.json_db import get_visit_pages
 
+def home(request):
+    return render(request, "home.html")
+
 # Checks is page exist, and if exists return page otherwise 404 page
 def page_existence(func):
     def wrapper(request, qr_id, *args, **kwargs):
         pages = get_visit_pages()
-        print(qr_id)
         if (str(qr_id) in pages):
             return func(request, qr_id, *args, **kwargs)
         else:
@@ -17,7 +19,7 @@ def page_existence(func):
     return wrapper
 
 @page_existence
-def home(request, qr_id):
+def visit(request, qr_id):
     pages = get_visit_pages()
     # Get requested page from json and convert to dataclass
     page = VisitPage(**pages[str(qr_id)])
