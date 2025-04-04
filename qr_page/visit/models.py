b08033418f551b6001ad7ac654b84585a467f494
JustNevi <122728617+JustNevi@users.noreply.github.com>
2025-04-04 12:17:24 +0300

    add dataclasses

--- qr_page/visit/models.py
+++ qr_page/visit/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class UrlButton:
+    redirect_url: str
+    text: str
+    icon_url: str
+
+@dataclass
+class VisitPage:
+    image_url: str
+    title: str
+    url_buttons: List[UrlButton]
+
+    def __post_init__(self):
+        self.url_buttons = [UrlButton(**url_button) for url_button in self.url_buttons]
