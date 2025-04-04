5d1342d751bb39b766b519df6cceeca1889db8e4
JustNevi <122728617+JustNevi@users.noreply.github.com>
2025-04-04 13:20:14 +0300

    refactor: rename url_buttons to redirect_buttons

--- qr_page/visit/models.py
+++ qr_page/visit/models.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from typing import List
 
 @dataclass
-class UrlButton:
+class RedirectButton:
     redirect_url: str
     text: str
     icon_url: str
@@ -13,7 +13,7 @@ class UrlButton:
 class VisitPage:
     image_url: str
     title: str
-    url_buttons: List[UrlButton]
+    url_buttons: List[RedirectButton]
 
     def __post_init__(self):
-        self.url_buttons = [UrlButton(**url_button) for url_button in self.url_buttons]
+        self.url_buttons = [RedirectButton(**url_button) for url_button in self.url_buttons]
