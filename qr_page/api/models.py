329965d72f2d7cd5ec9a1e623b7750c78cbaea90
JustNevi <122728617+JustNevi@users.noreply.github.com>
2025-06-03 22:56:35 +0200

    fix: fix model relations

--- qr_page/api/models.py
+++ qr_page/api/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+class VisitPage(models.Model):
+    title = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=200)
+
 class RedirectButton(models.Model):
+    visit_page = models.ForeignKey(VisitPage, on_delete=models.CASCADE, related_name="redirect_buttons") 
     redirect_url = models.CharField(max_length=200)
     text = models.CharField(max_length=15)
     icon_url = models.CharField(max_length=200)
 
-class VisitPage(models.Model):
-    title = models.CharField(max_length=200)
-    image_url = models.CharField(max_length=200)
-    redirect_button = models.ForeignKey(RedirectButton, on_delete=models.CASCADE) 
 
