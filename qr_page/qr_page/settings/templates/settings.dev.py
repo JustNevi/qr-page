383be6713041694607f0d50d3f6e599845a82d84
JustNevi <slavkad25@gmail.com>
2025-06-05 10:44:24 +0200

    chore: setup corsheaders for local tests

--- qr_page/qr_page/settings/templates/settings.dev.py
+++ qr_page/qr_page/settings/templates/settings.dev.py
@@ -1,2 +1,14 @@
 DEBUG = True
 SECRET_KEY = 'django-insecure-gdwlh_(gw&qhe6pncghs!s_z8f)!58kj_s&2d(h1@9@0@8&m$l'
+
+# Enable corsheaders for local tests
+
+LOCALHOST_PORT = 5173
+
+INSTALLED_APPS.append("corsheaders")
+
+CORS_ALLOWED_ORIGINS = [f"http://localhost:{LOCALHOST_PORT}"]
+
+# Put CorsMiddleware before CommonMiddleware
+common_middleware_index = MIDDLEWARE.index("django.middleware.common.CommonMiddleware")
+MIDDLEWARE.insert(common_middleware_index, "corsheaders.middleware.CorsMiddleware")
