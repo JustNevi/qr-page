ea3f9ca542e03607ee9efe9dcc839c0ede603102
JustNevi <122728617+JustNevi@users.noreply.github.com>
2025-06-07 18:42:57 +0200

    fix: cors allowed origins in setting prod template

--- qr_page/qr_page/settings/templates/settings.prod.py
+++ qr_page/qr_page/settings/templates/settings.prod.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}ALLOWED_HOSTS").split(',')
 ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS if host.strip()]
 
 CORS_ALLOWED_ORIGINS = os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}CORS_ALLOWED_ORIGINS").split(',')
-CORS_ALLOWED_ORIGINS = [host.strip() for host in ALLOWED_HOSTS if host.strip()]
+CORS_ALLOWED_ORIGINS = [host.strip() for host in CORS_ALLOWED_ORIGINS if host.strip()]
 
 # Put CorsMiddleware before CommonMiddleware
 common_middleware_index = MIDDLEWARE.index("django.middleware.common.CommonMiddleware")
