aaa55599b2e154fcb0cfb7dd42b2eff47a1edf99
JustNevi <122728617+JustNevi@users.noreply.github.com>
2025-06-07 18:16:24 +0200

    chore: add ALLOWED_HOSTS for prod. settings and change env importing

--- qr_page/qr_page/settings/templates/settings.prod.py
+++ qr_page/qr_page/settings/templates/settings.prod.py
@@ -7,7 +7,11 @@ SECRET_KEY = os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}SECRET_KEY")
 
 INSTALLED_APPS.append("corsheaders")
 
-CORS_ALLOWED_ORIGINS = json.loads(os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}ALLOWED_ORIGINS"))
+ALLOWED_HOSTS = os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}ALLOWED_HOSTS")).split(',')
+ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS if host.strip()]
+
+CORS_ALLOWED_ORIGINS = os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}CORS_ALLOWED_ORIGINS")).split(',')
+CORS_ALLOWED_ORIGINS = [host.strip() for host in ALLOWED_HOSTS if host.strip()]
 
 # Put CorsMiddleware before CommonMiddleware
 common_middleware_index = MIDDLEWARE.index("django.middleware.common.CommonMiddleware")
