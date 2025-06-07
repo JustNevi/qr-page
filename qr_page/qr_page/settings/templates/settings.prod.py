67074a287aff07b33cea985e48dfbff17753cbf6
JustNevi <slavkad25@gmail.com>
2025-06-07 18:25:17 +0200

    fix: extra ) in template prod settings

--- qr_page/qr_page/settings/templates/settings.prod.py
+++ qr_page/qr_page/settings/templates/settings.prod.py
@@ -5,10 +5,10 @@ SECRET_KEY = os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}SECRET_KEY")
 
 INSTALLED_APPS.append("corsheaders")
 
-ALLOWED_HOSTS = os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}ALLOWED_HOSTS")).split(',')
+ALLOWED_HOSTS = os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}ALLOWED_HOSTS").split(',')
 ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS if host.strip()]
 
-CORS_ALLOWED_ORIGINS = os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}CORS_ALLOWED_ORIGINS")).split(',')
+CORS_ALLOWED_ORIGINS = os.getenv(f"{ENV_VARS_SETTINGS_PREFIX}CORS_ALLOWED_ORIGINS").split(',')
 CORS_ALLOWED_ORIGINS = [host.strip() for host in ALLOWED_HOSTS if host.strip()]
 
 # Put CorsMiddleware before CommonMiddleware
